33e48e5694bd969ff43cb3cf6326068b96c6f7db
Arun Karthick Shankar <arunkarthick1422005@gmail.com>
2026-05-28 17:54:02 +0530

    Initial CI/CD project

---
+++ src/main/java/com/example/CiCdDemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.CiCdDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "CI/CD Pipeline Working Successfully!";
+    }
+}
